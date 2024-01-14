08345f8c63c7b1cf5c88066afcf5e052fc7907c0
Kacper Kielak <kielak.kacper@gmail.com>
2024-01-14 04:20:15 +0100

    Replace conv1d and basic torch primitives

--- model.py
+++ model.py
@@ -59,7 +59,7 @@ class Mamba(nn.Module):
         self.args = args
         
         self.embedding = nn.Embedding(args.vocab_size, args.d_model)
-        self.layers = nn.ModuleList([ResidualBlock(args) for _ in range(args.n_layer)])
+        self.layers = [ResidualBlock(args) for _ in range(args.n_layer)]
         self.norm_f = RMSNorm(args.d_model)
 
         self.lm_head = nn.Linear(args.d_model, args.vocab_size, bias=False)
@@ -119,7 +119,7 @@ class Mamba(nn.Module):
         def load_state_dict_hf(model_name, device=None, dtype=None):
             resolved_archive_file = cached_file(model_name, WEIGHTS_NAME,
                                                 _raise_exceptions_for_missing_entries=False)
-            return torch.load(resolved_archive_file, weights_only=True, map_location='cpu', mmap=True)
+            return mlx.load(resolved_archive_file, weights_only=True, map_location='cpu', mmap=True)
         
         config_data = load_config_hf(pretrained_model_name)
         args = ModelArgs(
@@ -181,14 +181,8 @@ class MambaBlock(nn.Module):
 
         self.in_proj = nn.Linear(args.d_model, args.d_inner * 2, bias=args.bias)
 
-        self.conv1d = nn.Conv1d(
-            in_channels=args.d_inner,
-            out_channels=args.d_inner,
-            bias=args.conv_bias,
-            kernel_size=args.d_conv,
-            groups=args.d_inner,
-            padding=args.d_conv - 1,
-        )
+        self.groups = args.d_inner
+        self.padding = args.d_conv - 1 
 
         # x_proj takes in `x` and outputs the input-specific Δ, B, C
         self.x_proj = nn.Linear(args.d_inner, args.dt_rank + args.d_state * 2, bias=False)
@@ -196,9 +190,9 @@ class MambaBlock(nn.Module):
         # dt_proj projects Δ from dt_rank to d_in
         self.dt_proj = nn.Linear(args.dt_rank, args.d_inner, bias=True)
 
-        A = repeat(torch.arange(1, args.d_state + 1), 'n -> d n', d=args.d_inner)
-        self.A_log = nn.Parameter(torch.log(A))
-        self.D = nn.Parameter(torch.ones(args.d_inner))
+        A = repeat(mlx.core.arange(1, args.d_state + 1), 'n -> d n', d=args.d_inner)
+        self.A_log = nn.Parameter(mlx.log(A))
+        self.D = nn.Parameter(mlx.ones(args.d_inner))
         self.out_proj = nn.Linear(args.d_inner, args.d_model, bias=args.bias)
         
 
@@ -222,7 +216,7 @@ class MambaBlock(nn.Module):
         (x, res) = x_and_res.split(split_size=[self.args.d_inner, self.args.d_inner], dim=-1)
 
         x = rearrange(x, 'b l d_in -> b d_in l')
-        x = self.conv1d(x)[:, :, :l]
+        x = self.conv1d(x, groups=self.groups, padding=self.padding)[:, :, :l]
         x = rearrange(x, 'b d_in l -> b l d_in')
         
         x = nn.silu(x)
@@ -258,7 +252,7 @@ class MambaBlock(nn.Module):
         #     ∆, B, C are input-dependent (this is a key difference between Mamba and the linear time invariant S4,
         #                                  and is why Mamba is called **selective** state spaces)
         
-        A = -torch.exp(self.A_log.float())  # shape (d_in, n)
+        A = -mlx.exp(self.A_log.float())  # shape (d_in, n)
         D = self.D.float()
 
         x_dbl = self.x_proj(x)  # (b, l, dt_rank + 2*n)
@@ -305,17 +299,17 @@ class MambaBlock(nn.Module):
         # - A is discretized using zero-order hold (ZOH) discretization (see Section 2 Equation 4 in the Mamba paper [1])
         # - B is discretized using a simplified Euler discretization instead of ZOH. From a discussion with authors:
         #   "A is the more important term and the performance doesn't change much with the simplification on B"
-        deltaA = torch.exp(einsum(delta, A, 'b l d_in, d_in n -> b l d_in n'))
+        deltaA = mlx.exp(einsum(delta, A, 'b l d_in, d_in n -> b l d_in n'))
         deltaB_u = einsum(delta, B, u, 'b l d_in, b l n, b l d_in -> b l d_in n')
         
         # Perform selective scan (see scan_SSM() in The Annotated S4 [2])
-        x = torch.zeros((b, d_in, n), device=deltaA.device)
+        x = mlx.zeros((b, d_in, n), device=deltaA.device)
         ys = []    
         for i in range(l):
             x = deltaA[:, i] * x + deltaB_u[:, i]
             y = einsum(x, C[:, i, :], 'b d_in n, b n -> b d_in')
             ys.append(y)
-        y = torch.stack(ys, dim=1)  # shape (b, l, d_in)
+        y = mlx.stack(ys, dim=1)  # shape (b, l, d_in)
         
         y = y + u * D
     
@@ -328,11 +322,11 @@ class RMSNorm(nn.Module):
                  eps: float = 1e-5):
         super().__init__()
         self.eps = eps
-        self.weight = nn.Parameter(torch.ones(d_model))
+        self.weight = nn.Parameter(mlx.ones(d_model))
 
 
     def forward(self, x):
-        output = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
+        output = x * mlx.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
 
         return output
-        
+       
